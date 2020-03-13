08f8e9efc6f31b419d962a014d7126b41d95eaea
Shmuel Levine <shmuel@thelevines.ca>
2020-03-13 13:37:19 -0400

    Further cleanup of backtesting and model modules
    - updated backtester class to suit revisions to model.  The data
      frequency and transforms are no longer passed separately to the
      Backtester ctor -- those are now encapsulated in the model object
    
    - Added test to ensure that the model object is of the correct type
    
    - Cleaned up whitespace
    
    - Added data transform to the Model class
    
    - Added test to ensure that data transform is either
      None (unlikely....) or is a callable -- i.e. function object

--- pymodules/foresight/backtesting.py
+++ pymodules/foresight/backtesting.py
@@ -1,26 +1,32 @@
 from pandas import Timedelta
+from foresight.model import Model as fx_Model
 
 class Backtester:
     """Class used for backtesting models against a timeseries"""
-    
-    def __init__(self, model, data_freq, data_transform, trading_rules, retraining_freq, initial_money = 10_000):
+
+    def __init__(self, model, trading_rules, retraining_freq, initial_money = 10_000):
         """Initialize a Backtester instance
-        
+
         Args:
             model (keras.Model) -- The fitted Keras Model to be backtested
-            data_freq (pandas.Timedelta) -- The frequency of the data used to fit the model
-            data_transform -- A function object used to transform the data used as a model input
-            trading_rules -- A subclass of Trading_Rules which contains rules used to determine when to make trades
-            retraining_frequency (pandas.Timedelta) -- A pandas.Timedelta object denoting how frequently to refit the model.
-                NOTE: at this time, the retraining will not be 100% exact.
-            initial_money -- An integer representing the amount of money initially used for trading. This amount is used
-                by the trading rules in establishing the value of each trade
+
+            trading_rules -- A subclass of Trading_Rules which contains rules used to
+               determine when to make trades
+
+            retraining_frequency (pandas.Timedelta) -- A pandas.Timedelta object denoting how
+                frequently to refit the model.  NOTE: at this time, the retraining will
+                not be 100% exact.
+
+            initial_money -- An integer representing the amount of money initially used
+                for trading. This amount is used by the trading rules in establishing the
+                value of each trade
+
         """
-        
+
+        if model is None or not isinstance(model,fx_Model):
+            raise TypeError('param model: Must be type foresight.model.Model')
         self.model = model
-#        self.data_transform = data_transform
         self.trading_rules = trading_rules
-        #self.retraining_freq = retraining_freq
         self.initial_money = initial_money
 
         # Calculate the retraining frequency (in number of datapoints)
@@ -28,24 +34,16 @@ class Backtester:
         if type(data_freq) is not Timedelta:
             print("Invalid data frequency provided. Must be of type pandas.Timedelta")
             raise TypeError
-            
+
         if type(retraining_freq) is not Timedelta:
             print("Invalid retraining frequency provided. Must be of type pandas.Timedelta")
             raise TypeError
-            
+
         self.retrain_freq =  retraining_freq / data_freq
         print("Restarting every", self.retrain_freq, "datapoints")
         print("New backtester created!!")
-        
+
     def Backtest(self, data, verbose=False):
         cur_idx = 0
         print("Retraining every", self.retrain_freq, "points")
         pass
-    
-    
-    
-    
-    
-    
-    
-    
--- pymodules/foresight/model.py
+++ pymodules/foresight/model.py
@@ -5,18 +5,21 @@ import numpy
 import pandas
 from numpy import array as np_array
 from  pandas import Timedelta as TD
+from collections import Callable
 
 class Model:
     """ Defines a class which encapsulates a Keras.Model object used for backtesting and trading"""
-    def __init__(self, model, data, data_freq):
-        if (model is None) or (not isinstance(model, tf_model)): 
+    def __init__(self, model, data, data_freq, data_transform = None):
+        if (model is None) or (not isinstance(model, tf_model)):
             raise TypeError(' \'model\' must be an instance of tensorflow.keras.Model')
-        if (data is None) or (not isinstance(data, numpy.ndarray)): 
+        if (data is None) or (not isinstance(data, numpy.ndarray)):
             raise TypeError('\'data\' must be an instance of numpy.array')
-        if (data_freq is None) or (not isinstance(data_freq, pandas.Timedelta)): 
+        if (data_freq is None) or (not isinstance(data_freq, pandas.Timedelta)):
             raise TypeError('\'data_freq\' must be an instance of pandas.Timedelta ')
-
+        if (data_transform is not None) and (not callable(data_transform)):
+            raise TypeError('data_transform must either be a function object or None')
         self.model = model
         self.data = data
         self.data_freq = data_freq
-        
+        self.data_transform = data_transform
+
